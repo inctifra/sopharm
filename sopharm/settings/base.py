d17128f60b1ecea037efaa6b8927ff8ceaaa7695
dlion4 <dantemilimo@gmail.com>
2025-12-07 15:00:39 +0300

    fixed the debug

--- sopharm/settings/base.py
+++ sopharm/settings/base.py
@@ -99,12 +99,8 @@ WSGI_APPLICATION = "sopharm.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
+DATABASES = {"default": env.db("DATABASE_URL", default="sqlite:///db.sqlite3")}
+
 
 
 # Password validation
